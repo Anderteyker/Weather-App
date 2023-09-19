de3292bb7e8d6a40de36243c7e9d71dfdde46843
AndreyP <mranderteyker@gmail.com>
2023-09-19 21:41:33 +0300

    d1.5

---
+++ index.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('.container');
+const container = document.querySelector('.container');
+const container = document.querySelector('.container');
+const container = document.querySelector('.container');
+const container = document.querySelector('.container');
